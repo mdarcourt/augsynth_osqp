db74b1509960d51a11297842ee2a0c0e2743c445
mdarcourt <mdarcourt@crai.com>
2025-11-18 20:03:14 -0800

    Refactor fit_synth_formatted to improve output and clarity
    Refactored fit_synth_formatted function to use solution from synth_qp directly and return warm_status.

--- R/fit_synth.R
+++ R/fit_synth.R
@@ -36,29 +36,31 @@ make_V_matrix <- function(t0, V) {
 #' }
 fit_synth_formatted <- function(synth_data, V = NULL, warm_start = NULL) {
 
-    t0 <- dim(synth_data$Z0)[1]
-    ## if no V is supplied, set equal to I
+  t0 <- dim(synth_data$Z0)[1]
+  V  <- make_V_matrix(t0, V)
 
-    V <- make_V_matrix(t0, V)
+  sol <- synth_qp(
+    X1 = synth_data$X1,
+    X0 = t(synth_data$X0),
+    V  = V,
+    warm_start = warm_start
+  )
 
-    weights <- synth_qp(
-      X1         = synth_data$X1,
-      X0         = t(synth_data$X0),
-      V          = V,
-      warm_start = warm_start   # <--- NEW
-    )
+  weights <- sol$x
 
-    l2_imbalance <- sqrt(sum((synth_data$Z0 %*% weights - synth_data$Z1)^2))
+  l2_imbalance <- sqrt(sum((synth_data$Z0 %*% weights - synth_data$Z1)^2))
+  uni_w <- matrix(1/ncol(synth_data$Z0), nrow=ncol(synth_data$Z0), ncol=1)
+  unif_l2_imbalance <- sqrt(sum((synth_data$Z0 %*% uni_w - synth_data$Z1)^2))
+  scaled_l2_imbalance <- l2_imbalance / unif_l2_imbalance
 
-    ## primal objective value scaled by least squares difference for mean
-    uni_w <- matrix(1/ncol(synth_data$Z0), nrow=ncol(synth_data$Z0), ncol=1)
-    unif_l2_imbalance <- sqrt(sum((synth_data$Z0 %*% uni_w - synth_data$Z1)^2))
-    scaled_l2_imbalance <- l2_imbalance / unif_l2_imbalance
-
-    return(list(weights = weights,
-                l2_imbalance = l2_imbalance,
-                scaled_l2_imbalance = scaled_l2_imbalance))
+  return(list(
+    weights = weights,
+    warm_status = sol$warm_status,
+    l2_imbalance = l2_imbalance,
+    scaled_l2_imbalance = scaled_l2_imbalance
+  ))
 }
+
 
 
 #' Solve the synth QP directly
