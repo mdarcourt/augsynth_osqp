b6d6dc49ba1b03587d6225878cdccdaa54e5a6b7
mdarcourt <mdarcourt@crai.com>
2025-11-18 19:39:54 -0800

    Increase OSQP tolerance settings for optimization

--- R/fit_synth.R
+++ R/fit_synth.R
@@ -80,8 +80,8 @@ synth_qp <- function(X1, X0, V, warm_start = NULL) {
     # Turn on verbose so OSQP prints progress (you will SEE warm-start effects)
     settings <- osqp::osqpSettings(
       verbose = TRUE,
-      eps_rel = 1e-8,
-      eps_abs = 1e-8
+      eps_rel = 1e-3,
+      eps_abs = 1e-3
     )
 
     # Build OSQP model
